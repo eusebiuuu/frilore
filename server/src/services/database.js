05e6fbf77dc7da288531b51024bbcf8f8bda7545
Eusebiu <eusebiu.rimboi04@gmail.com>
2024-09-02 11:48:41 +0300

    Change database connection code structure and add install command on package.json

--- server/src/services/database.js
+++ server/src/services/database.js
@@ -9,25 +9,28 @@ const { Pool } = pkg;
 export const connectionData = {
   user: process.env.POSTGRES_USER,
   host: process.env.POSTGRES_HOST,
-  database: 'frilore',
+  database: process.env.POSTGRES_DATABASE_NAME,
   password: process.env.POSTGRES_PASSWORD,
   port: process.env.POSTGRES_PORT,
-  ssl: true,
+  ssl: false,
 }
 
 const __filename = fileURLToPath(import.meta.url);
 
 const __dirname = path.dirname(__filename);
 
-const pool = new Pool({
-  user: process.env.POSTGRES_USER,
-  host: process.env.POSTGRES_HOST,
-  database: 'frilore',
-  password: process.env.POSTGRES_PASSWORD,
-  port: process.env.POSTGRES_PORT,
-  ssl: {
-    ca: fs.readFileSync(path.resolve(__dirname, '../../cert/eu-north-1-bundle.pem')),
-  },
-});
+const pool = new Pool(connectionData);
+
+
+// const pool = new Pool({
+//   user: process.env.POSTGRES_USER,
+//   host: process.env.POSTGRES_HOST,
+//   database: 'frilore',
+//   password: process.env.POSTGRES_PASSWORD,
+//   port: process.env.POSTGRES_PORT,
+//   ssl: {
+//     ca: fs.readFileSync(path.resolve(__dirname, '../../cert/eu-north-1-bundle.pem')),
+//   },
+// });
 
 export default pool;
